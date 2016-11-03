e0a7aedab574f51eb49244e965c7faf8965d7c05
Benjamin Orsini <benjamin.orsini.dev@gmail.com>
2016-11-03 18:25:13 +0100

    Refacto : separating in multiple components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent, NumberToArray }   from './app.component';
+import { AppComponent, TransactionComponent, PostingComponent, NumberToArray }   from './app.component';
 import { LedgerService }        from './ledgerservice';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, NumberToArray ],
+  declarations: [ AppComponent, TransactionComponent, PostingComponent, NumberToArray ],
   bootstrap:    [ AppComponent ],
   providers:    [ LedgerService ]
 })
