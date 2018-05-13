48eece29e810550f04a21187ea168079e5c3dc6f
Benjamin <benjamin.orsini.dev@gmail.com>
2018-05-13 15:10:12 +0200

    test(selector): add unit tests

--- src/app/shared/selectors/__tests__/selectors.test.ts
+++ src/app/shared/selectors/__tests__/selectors.test.ts
@@ -1,10 +1,11 @@
 import * as moment from 'moment';
 
-import { accountsFiltered } from '../selectors';
+import { accountsFiltered, editedTransactionSelector, selectedTransactionsSelector, tabsSelector } from '../selectors';
 import { AccountMap } from '../../models/app-state';
 import { rootReducer, AppStateActions } from '../../reducers/app-state-reducer';
+import { Transaction } from '../../models/transaction';
 
-const transactions = [ 
+const transactionsFixture = [ 
   {
     header: {
       date: moment.utc('2013-02-08').unix(),
@@ -22,6 +23,24 @@ const transactions = [
   }
 ];
 
+jest.mock('uuid', () => {
+  let counter = 0;
+
+  return {
+    v4: jest.fn(() => {
+      const uuid = `uuid ${counter}`;
+      counter++;
+      return uuid;
+    }),
+    reset: () => counter = 0,
+  };
+});
+
+
+beforeEach( () => {
+  require('uuid').reset();
+});
+
 describe(accountsFiltered.name, () => {
   it('returns root by default', () => {
     const state = rootReducer(undefined, AppStateActions.addTransactions([]));
@@ -32,7 +51,7 @@ describe(accountsFiltered.name, () => {
   });
 
   it('returns all accounts by default', () => {
-    const state = rootReducer(undefined, AppStateActions.addTransactions(transactions));
+    const state = rootReducer(undefined, AppStateActions.addTransactions(transactionsFixture));
     
     const result = accountsFiltered('')(state);
 
@@ -40,11 +59,37 @@ describe(accountsFiltered.name, () => {
   });
 
   it('filters correctly', () => {
-    const state = rootReducer(undefined, AppStateActions.addTransactions(transactions));
+    const state = rootReducer(undefined, AppStateActions.addTransactions(transactionsFixture));
     
     const result = accountsFiltered('B')(state);
 
     expect(Object.keys(result).sort()).toEqual(['ROOT', 'A', 'A:B', 'A:B:C', 'A:B:D'].sort());
+  });
+});
+
+describe(editedTransactionSelector.name, () => {
+  it('returns transaction with uuid', () => {
+    const state = rootReducer(undefined, AppStateActions.addTransactions(transactionsFixture));
+    const state2 = rootReducer(state, AppStateActions.openTab(['A']));
+    const firstTab = Object.keys(tabsSelector(state2))[0];
+    const transactions = selectedTransactionsSelector(firstTab)(state2);
+    const firstTransaction = transactions[0];
+    const state3 = rootReducer(state, AppStateActions.setEditedTransaction(firstTransaction, firstTab));
+
+    const result = editedTransactionSelector(firstTab)(state3);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('returns transaction with no uuid', () => {
+    const state = rootReducer(undefined, AppStateActions.openTab(['A']));
+    const firstTab = Object.keys(tabsSelector(state))[0];
+    const newTransaction = transactionsFixture[0];
+    const state3 = rootReducer(state, AppStateActions.setEditedTransaction(newTransaction, firstTab));
+
+    const result = editedTransactionSelector(firstTab)(state3);
+
+    expect(result).toEqual(newTransaction);
   });
 });
 
