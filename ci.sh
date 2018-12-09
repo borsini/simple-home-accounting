28bfafbf2261401d548aec198b8e2fb65b6ec698
Benjamin <benjamin.orsini.dev@gmail.com>
2018-12-09 14:58:55 +0100

    ci: fix

--- ci.sh
+++ ci.sh
@@ -10,10 +10,15 @@ ci_build_prod()
     yarn ng build --prod
 }
 
+
+ci_clean()
+{
+    rm -rf build
+    rm -rf dist
+}
+
 ci_compile_windows()
 {
-    ci_build_prod
-
     # docker run parameters : 
     # tty
     # inject environment variables
@@ -30,22 +35,23 @@ ci_compile_windows()
     --rm \
     -ti \
     --env-file <(env | grep -vE '\r|\n' | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+    --env ELECTRON_CACHE="/root/.cache/electron" \
+    --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
     -v ${PWD}/dist:/project/dist \
     -v ${PWD}:/project \
+    -v ${PWD##*/}-node-modules:/project/node_modules \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/builder:wine \
-    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn electron-builder build --win" 
+    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn electron-builder build --win --publish never" 
 }
 
 ci_compile_mac()
 {
-    ci_build_prod
-    yarn electron-builder build --mac
+    yarn electron-builder build --mac --publish never
 }
 
 ci_deploy_firebase()
 {
-    ci_build_prod
     yarn firebase deploy --token $FIREBASE_TOKEN
 }
